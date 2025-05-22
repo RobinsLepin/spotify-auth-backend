9e50fdec84d1bdc66c9e52b6e1972e2b57e9d479
RobinsLepin <105516181+RobinsLepin@users.noreply.github.com>
2025-05-22 18:21:34 -0400

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const client_id = "74dc21db45b74ca19665f9ff2d334554";
 const client_secret = "57cd816cf7c04d71b5a7918391412acc";
-const redirect_uri = "https://spotify-auth-backend-wi52.vercel.app/";
+const redirect_uri = "https://spotify-auth-backend-wi52.vercel.app/callback";
 
 export default async function handler(req, res) {
   // Manejar CORS
